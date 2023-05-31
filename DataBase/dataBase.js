38c6e7301f094b9132e073b57a66748a94c0c353
mahmoudElali <mahmud.elali98@gmail.com>
2023-05-31 17:02:23 +0300

    test database connection

--- DataBase/dataBase.js
+++ DataBase/dataBase.js
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-mongoose.set("strictQuery", true);
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+mongoose.set('strictQuery', true);
 dotenv.config();
 
 const connectToDatabase = async () => {
-	try {
-		await mongoose
-			.connect(process.env.MONGODB_URL, { useUnifiedTopology: true })
-			.then(() => {
-				console.log(`connected to database`);
-			});
-	} catch (error) {
-		console.log(error.message);
-	}
+	await mongoose
+		.connect(process.env.MONGODB_URL, { useUnifiedTopology: true })
+		.then(() => {
+			console.log(`connected to database`);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 };
 
 export default connectToDatabase;
